ffa89c1a5b006679e3aafb0d51d492c58e09b618
HyemCha <chm6194@gmail.com>
2022-09-28 23:06:43 +0900

    ConstraintLayout

---
+++ ConstraintLayout_Ex01/app/src/main/java/com/example/constraintlayout_ex01/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.constraintlayout_ex01;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
