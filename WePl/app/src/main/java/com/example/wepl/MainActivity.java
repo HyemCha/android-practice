308213df30ec6d9d3074b2ed22e9285d5c93d20c
HyemCha <chm6194@gmail.com>
2022-10-12 22:28:56 +0900

    build.gradle(:app ):add a dependency on Room

---
+++ WePl/app/src/main/java/com/example/wepl/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.wepl;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity{
+    private static final String TAG = "MusicService";
+    Button start, stop;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+}
